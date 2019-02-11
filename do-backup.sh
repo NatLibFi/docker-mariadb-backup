c8131245ceb121b6768ba54fd01d46afeae092d7
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-02-11 14:04:33 +0200

    Require backup prefix for filename

--- do-backup.sh
+++ do-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-DUMP_FILE=$DUMP_DIRECTORY/`date +%d%m%yT%H%M.sql`
+DUMP_FILE=${DUMP_DIRECTORY}/${BACKUP_PREFIX}-`date +%d%m%yT%H%M.sql`
 
 echo -n "Dumping database to ${DUMP_FILE}..."
 /usr/bin/mysqldump -h $DB_HOST --protocol=tcp -u $DB_USERNAME --password="$DB_PASSWORD" $DB_NAME > $DUMP_FILE 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 trap 'exit' 2
+
+if [ -z "$BACKUP_PREFIX" ];then
+  echo "Variable BACKUP_PREFIX is mandatory"
+  exit 255
+fi
+
 echo "Starting eternal sleep..."
 
 while :
