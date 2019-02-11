26247fbae2e8410f077632e1f4ff8c5f48feb9d2
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-02-11 13:55:22 +0200

    Fix message

--- do-backup.sh
+++ do-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e 
 
-echo -n "Dumping database to file ${DUMP_DIRECTORY}..."
-/usr/bin/mysqldump -h $DB_HOST --protocol=tcp -u $DB_USERNAME --password="$DB_PASSWORD" $DB_NAME > $DUMP_DIRECTORY/`date +%d%m%yT%H%M.sql`
+DUMP_FILE=$DUMP_DIRECTORY/`date +%d%m%yT%H%M.sql`
+
+echo -n "Dumping database to ${DUMP_FILE}..."
+/usr/bin/mysqldump -h $DB_HOST --protocol=tcp -u $DB_USERNAME --password="$DB_PASSWORD" $DB_NAME > $DUMP_FILE 
 echo "Done."
