9ab079b5c2e4c76009f0ab9c9421a0d83db02945
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-02-12 10:52:13 +0200

    Add `--single-transaction` to backup command

--- do-backup.sh
+++ do-backup.sh
@@ -4,5 +4,5 @@ set -e
 DUMP_FILE=${DUMP_DIRECTORY}/${BACKUP_PREFIX}-`date +%d%m%yT%H%M.sql`
 
 echo -n "Dumping database to ${DUMP_FILE}..."
-/usr/bin/mysqldump -h $DB_HOST --protocol=tcp -u $DB_USERNAME --password="$DB_PASSWORD" $DB_NAME > $DUMP_FILE 
+/usr/bin/mysqldump -h $DB_HOST --protocol=tcp -u $DB_USERNAME --password="$DB_PASSWORD" --single-transaction $DB_NAME > $DUMP_FILE 
 echo "Done."
